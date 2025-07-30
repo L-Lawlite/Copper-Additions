7e1432f64f6e08b4d20a4cc766feffbf67be4ff6
Lawliet <junirobert21@gmail.com>
2025-07-30 20:50:51 +0530

    Rendering fix

--- src/main/java/net/lawliet/copper_additions/utility/EquipmentAssetsPath.java
+++ src/main/java/net/lawliet/copper_additions/utility/EquipmentAssetsPath.java
@@ -7,7 +7,7 @@ import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.equipment.EquipmentAsset;
 
 public interface EquipmentAssetsPath {
-    ResourceKey<? extends Registry<EquipmentAsset>> ROOT_ID = ResourceKey.createRegistryKey(ResourceLocation.fromNamespaceAndPath(CopperAdditions.MODID, "equipment_asset"));
+    ResourceKey<? extends Registry<EquipmentAsset>> ROOT_ID = ResourceKey.createRegistryKey(ResourceLocation.withDefaultNamespace("equipment_asset"));
 
     ResourceKey<EquipmentAsset> COPPER = ResourceKey.create(ROOT_ID, ResourceLocation.fromNamespaceAndPath(CopperAdditions.MODID, "copper"));
 
