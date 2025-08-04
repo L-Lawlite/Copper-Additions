aed201172fe138f2a1bf552ad7d8e43e312b7d80
Lawliet <junirobert21@gmail.com>
2025-08-04 09:53:47 +0530

    Tool Material class name fixed

--- src/main/java/net/lawliet/copper_additions/copperAdditionsRegistration/Items.java
+++ src/main/java/net/lawliet/copper_additions/copperAdditionsRegistration/Items.java
@@ -2,7 +2,7 @@ package net.lawliet.copper_additions.copperAdditionsRegistration;
 
 import net.lawliet.copper_additions.CopperAdditions;
 import net.lawliet.copper_additions.utility.ArmorMaterialsAddition;
-import net.lawliet.copper_additions.utility.TootMaterialAddition;
+import net.lawliet.copper_additions.utility.ToolMaterialAddition;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.world.item.*;
 import net.minecraft.world.item.equipment.ArmorType;
@@ -26,11 +26,11 @@ public class Items {
 
     static {
         COPPER_NUGGET = CopperAdditions.ITEMS.registerSimpleItem("copper_nugget");
-        COPPER_SWORD = CopperAdditions.ITEMS.registerItem("copper_sword",properties ->  new SwordItem(TootMaterialAddition.COPPER, 3.0F, -2.4F, properties));
-        COPPER_SHOVEL = CopperAdditions.ITEMS.registerItem("copper_shovel", properties -> new ShovelItem(TootMaterialAddition.COPPER, 1.5F, -3.0F, properties));
-        COPPER_PICKAXE = CopperAdditions.ITEMS.registerItem("copper_pickaxe",properties ->  new PickaxeItem(TootMaterialAddition.COPPER, 1.0F, -2.8F, properties));
-        COPPER_AXE = CopperAdditions.ITEMS.registerItem("copper_axe", properties -> new AxeItem(TootMaterialAddition.COPPER, 7.0F, -3.2F, properties));
-        COPPER_HOE = CopperAdditions.ITEMS.registerItem("copper_hoe", properties -> new HoeItem(TootMaterialAddition.COPPER, -1.0F, -2.0F, properties));
+        COPPER_SWORD = CopperAdditions.ITEMS.registerItem("copper_sword",properties ->  new SwordItem(ToolMaterialAddition.COPPER, 3.0F, -2.4F, properties));
+        COPPER_SHOVEL = CopperAdditions.ITEMS.registerItem("copper_shovel", properties -> new ShovelItem(ToolMaterialAddition.COPPER, 1.5F, -3.0F, properties));
+        COPPER_PICKAXE = CopperAdditions.ITEMS.registerItem("copper_pickaxe",properties ->  new PickaxeItem(ToolMaterialAddition.COPPER, 1.0F, -2.8F, properties));
+        COPPER_AXE = CopperAdditions.ITEMS.registerItem("copper_axe", properties -> new AxeItem(ToolMaterialAddition.COPPER, 7.0F, -3.2F, properties));
+        COPPER_HOE = CopperAdditions.ITEMS.registerItem("copper_hoe", properties -> new HoeItem(ToolMaterialAddition.COPPER, -1.0F, -2.0F, properties));
 
         COPPER_HELMET = CopperAdditions.ITEMS.registerItem("copper_helmet", properties -> new ArmorItem(ArmorMaterialsAddition.COPPER, ArmorType.HELMET, properties));
         COPPER_CHESTPLATE = CopperAdditions.ITEMS.registerItem("copper_chestplate", properties -> new ArmorItem(ArmorMaterialsAddition.COPPER, ArmorType.CHESTPLATE, properties));
@@ -40,7 +40,7 @@ public class Items {
     }
 
     public static void init() {
-        TootMaterialAddition.init();
+        ToolMaterialAddition.init();
     }
 
     @SuppressWarnings("unused")
