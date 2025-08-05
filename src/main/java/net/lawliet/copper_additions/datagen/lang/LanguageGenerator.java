a576fa53630073ef5a761e8dbb67082b21e41808
Lawliet <junirobert21@gmail.com>
2025-08-05 16:49:19 +0530

    Added lang file

--- src/main/java/net/lawliet/copper_additions/datagen/lang/LanguageGenerator.java
+++ src/main/java/net/lawliet/copper_additions/datagen/lang/LanguageGenerator.java
@@ -1,6 +1,7 @@
 package net.lawliet.copper_additions.datagen.lang;
 
 import net.lawliet.copper_additions.CopperAdditions;
+import net.lawliet.copper_additions.copperAdditionsRegistration.Items;
 import net.minecraft.data.PackOutput;
 import net.neoforged.neoforge.common.data.LanguageProvider;
 
@@ -11,6 +12,17 @@ public class LanguageGenerator extends LanguageProvider {
 
     @Override
     protected void addTranslations() {
+        this.addItem(Items.COPPER_NUGGET,"Copper Nugget");
+        this.addItem(Items.COPPER_SWORD, "Copper Sword");
+        this.addItem(Items.COPPER_SHOVEL, "Copper Shovel");
+        this.addItem(Items.COPPER_PICKAXE, "Copper Pickaxe");
+        this.addItem(Items.COPPER_AXE, "Copper Axe");
+        this.addItem(Items.COPPER_HOE, "Copper Hoe");
+        this.addItem(Items.COPPER_HELMET, "Copper Helmet");
+        this.addItem(Items.COPPER_CHESTPLATE, "Copper Chestplate");
+        this.addItem(Items.COPPER_LEGGINGS, "Copper Leggings");
+        this.addItem(Items.COPPER_BOOTS, "Copper Boots");
+        this.addItem(Items.COPPER_HORSE_ARMOR, "Copper Horse Armor");
 
     }
 }
