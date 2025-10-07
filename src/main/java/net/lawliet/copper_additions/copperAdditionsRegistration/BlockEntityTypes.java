607c7e2ff4fee5e81dbcc2427132b11fc77b05b6
Lawliet <junirobert21@gmail.com>
2025-10-08 04:02:16 +0530

    Removed copper chest from game not from code

--- src/main/java/net/lawliet/copper_additions/copperAdditionsRegistration/BlockEntityTypes.java
+++ src/main/java/net/lawliet/copper_additions/copperAdditionsRegistration/BlockEntityTypes.java
@@ -7,15 +7,15 @@ import net.minecraft.world.level.block.entity.BlockEntityType;
 import java.util.function.Supplier;
 
 public class BlockEntityTypes {
-    public static final Supplier<BlockEntityType<CopperChestBlockEntity>> COPPER_CHEST_BLOCK_ENTITY;
+//    public static final Supplier<BlockEntityType<CopperChestBlockEntity>> COPPER_CHEST_BLOCK_ENTITY;
 
 
     static {
-        COPPER_CHEST_BLOCK_ENTITY = CopperAdditions.BLOCK_ENTITY_TYPES.register("copper_block_entity", () -> new BlockEntityType<>(
-                CopperChestBlockEntity::new,
-                false,
-                Blocks.COPPER_CHEST.get()
-        ));
+//        COPPER_CHEST_BLOCK_ENTITY = CopperAdditions.BLOCK_ENTITY_TYPES.register("copper_block_entity", () -> new BlockEntityType<>(
+//                CopperChestBlockEntity::new,
+//                false,
+//                Blocks.COPPER_CHEST.get()
+//        ));
     }
 
 
